a8020325fc579dbb8e7f928c47897972bfd06e12
Izhar Ali <aliizh94@students.rowan.edu>
2024-02-01 21:42:27 -0500

    connecting flask with Jinja

--- app/views/main.py
+++ app/views/main.py
@@ -1,22 +1,19 @@
 from flask import Blueprint, render_template, session
-from flask_login import current_user, login_user
-from app.data import PLANS, MODELS
-from app.services import get_user_by_email
-from .auth import google_auth
 
 main_bp = Blueprint('main', __name__, url_prefix='/')
 
 @main_bp.route('/')
 def index():
-    session.permanent = False
-    if 'oauth_token' in session:
-        info = google_auth.authorized_response()
-        email = info['email']
-        user = get_user_by_email(email)
-        if user:
-            login_user(user)
-    return render_template("index.html", user=current_user,  models=MODELS)
+    return render_template('index.html')
 
-@main_bp.route('/status')
-def status():
-    return 'OK', 200
+@main_bp.route('/studio')
+def studio():
+    return render_template('studio.html')
+
+@main_bp.route('/gallery')
+def gallery():
+    return render_template('gallery.html')
+
+@main_bp.route('/docs')
+def docs():
+    return render_template('docs.html')
