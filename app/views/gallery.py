56a4ae33b0c02e5d0fe9b4e39ca33a5a47b53c25
Izhar Ali <aliizh94@students.rowan.edu>
2024-02-01 22:29:47 -0500

    removed old views that were lorem ipsum

--- app/views/gallery.py
+++ app/views/gallery.py
@@ -1,28 +1,3 @@
-import os
-import random
-import base64
-import traceback
-import logging
-
-# define gallery blueprint
-gallery_bp = Blueprint('gallery', __name__, url_prefix='/gallery')
-
-
-@gallery_bp.route('/')
-def gallery():
-    # get all gallery images
-    images = [f for f in os.listdir('app/static/img') if f.endswith('.png')]
-    return render_template('gallery.html', images=images)
-
-
-@gallery_bp.route('/gallery_image/<path:img_name>', methods=['GET'])
-def gallery_image(img_name):
-    try:
-        file_path = f"app/static/img/{img_name}"
-        with open(file_path, "rb") as img_file:
-            image = base64.b64encode(img_file.read()).decode('utf-8')
-        image_data = f"data:image/png;base64,{image}"
-        return render_template("result.html", image=image_data, prompt="Gallery Image", user=current_user)
-    except Exception as e:
-        logging.error(f"[Function: gallery_image] Error serving image: {e}")
-        return render_template("error.html")
+"""
+This file contains the gallery view.
+"""
--- app/views/studio.py
+++ app/views/studio.py
@@ -1,22 +1,3 @@
-from flask import Blueprint, render_template, session
-from flask_login import current_user, login_user
-from app.data import PLANS, MODELS
-from app.services import get_user_by_email
-from .auth import google_auth
-
-main_bp = Blueprint('main', __name__, url_prefix='/')
-
-@main_bp.route('/')
-def index():
-    session.permanent = False
-    if 'oauth_token' in session:
-        info = google_auth.authorized_response()
-        email = info['email']
-        user = get_user_by_email(email)
-        if user:
-            login_user(user)
-    return render_template("index.html", user=current_user,  models=MODELS)
-
-@main_bp.route('/status')
-def status():
-    return 'OK', 200
+"""
+This file contains the studio view.
+"""
